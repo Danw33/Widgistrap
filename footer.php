6cd24253b6a41d1e6dc059df9545940e58f32fb1
Daniel Wilson <dan.wilson@acuica.co.uk>
2014-03-24 00:47:21 +0000

    Added translatable footer and almost-bootstrap header Navigation Bar

--- footer.php
+++ footer.php
@@ -1,3 +1,12 @@
-<?php wp_footer(); ?>
+<footer id="footer" role="contentinfo">
+    <div id="copyright">
+	<?php
+	echo sprintf( __( '%1$s %2$s %3$s. All Rights Reserved.' , 'wigistrap' ) , '&copy;' , date( 'Y' ) , esc_html( get_bloginfo( 'name' ) ) ) ;
+	echo sprintf( __( ' Theme By: %1$s.' , 'wigistrap' ) , '<a href="http://www.danielwilson.me/">Daniel Wilson</a>' ) ;
+	?>
+    </div>
+</footer>
+</div><!-- /#outer-container -->
+<?php wp_footer() ; ?>
 </body>
 </html>
--- header.php
+++ header.php
@@ -1,11 +1,34 @@
 <!DOCTYPE html>
-<html <?php language_attributes(); ?>>
+<html <?php language_attributes() ; ?>>
     <head>
-        <meta charset="<?php bloginfo('charset'); ?>" />
+        <meta charset="<?php bloginfo( 'charset' ) ; ?>" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title><?php wp_title(' | ', true, 'right'); ?></title>
-        <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>" />
-        <?php wp_head(); ?>
+        <title><?php wp_title( ' | ' , true , 'right' ) ; ?></title>
+        <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri() ; ?>" />
+	<?php wp_head() ; ?>
     </head>
-    <body <?php body_class(); ?>>
+    <body <?php body_class() ; ?>>
+        <header id="header">
+	    <nav class="navbar navbar-default" role="navigation">
+		<div class="container-fluid">
+		    <div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="navbar-collapse">
+			    <span class="sr-only">Toggle navigation</span>
+			    <span class="icon-bar"></span>
+			    <span class="icon-bar"></span>
+			    <span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ) ; ?>" title="<?php esc_attr_e( get_bloginfo( 'name' ) , 'wigistrap' ) ; ?>" rel="home"><?php echo esc_html( get_bloginfo( 'name' ) ) ; ?></a>
+		    </div>
+		    <div class="collapse navbar-collapse" id="navbar-collapse">
+			<?php
+			wp_nav_menu( array(
+			    'theme_location' => 'main-menu' ) ) ;
+			get_search_form() ;
+			?>
+		    </div><!-- /.navbar-collapse -->
+		</div><!-- /.container-fluid -->
+	    </nav>
+        </header>
+        <div id="outer-container">
