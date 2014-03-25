959d48ffd4e50fb9b451498dd109644839477cc0
Daniel Wilson <dan.wilson@acuica.co.uk>
2014-03-25 20:51:45 +0000

    Updated sidebar to fallback to a simple search-box sidebar if it's not configured.

--- sidebar.php
+++ sidebar.php
@@ -1,9 +1,11 @@
 <aside id="sidebar" role="complementary">
-
     <div id="primary" class="widget-area">
 	<ul class="widget-area-list">
-	    <?php dynamic_sidebar( 'primary-right' ); ?>
+	    <?php if( ! dynamic_sidebar( 'primary-widget-area' ) ) : ?>
+    	    <li id="search" class="primary-widget-area-widget widget widget-container widget_search">
+		    <?php get_search_form(); ?>
+    	    </li>
+	    <?php endif; ?>
 	</ul>
     </div>
-
 </aside>
