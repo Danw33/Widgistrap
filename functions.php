0cfa9fc70753773498e28a57829c7bdfa32e3e73
Daniel Wilson <dan.wilson@acuica.co.uk>
2014-03-25 23:58:25 +0000

    Updated widgets to be in bootstrap panels. Started playing with wp_customize

--- functions.php
+++ functions.php
@@ -19,7 +19,7 @@
 
 /**
  * Register the sidebars and widgetized areas.
- * (Including the Widgipage Bars)
+ * (Including the Widgipage Bars, all as Bootstrap Panels)
  */
 function widgistrap_widgets_init() {
 
@@ -27,29 +27,29 @@ function widgistrap_widgets_init() {
     register_sidebar( array(
 	'name' => 'Primary Right sidebar' ,
 	'id' => 'primary-widget-area' ,
-	'before_widget' => '<div class="primary-widget-area-widget widget">' ,
+	'before_widget' => '<div class="panel panel-default panel-widget">' ,
 	'after_widget' => '</div>' ,
-	'before_title' => '<h2 class="rounded">' ,
-	'after_title' => '</h2>' ,
+	'before_title' => '<div class="panel-heading"><h3 class="panel-title rounded">' ,
+	'after_title' => '</h3></div><div class="panel-body">' ,
     ) );
 
     //Two- and Three-Column Widgipage
     register_sidebar( array(
 	'name' => 'Widgipage First Column' ,
 	'id' => 'widgipage-col-1' ,
-	'before_widget' => '<div class="widgipage-col-1-widget widget">' ,
+	'before_widget' => '<div class="panel panel-default panel-widget">' ,
 	'after_widget' => '</div>' ,
-	'before_title' => '<h2 class="rounded">' ,
-	'after_title' => '</h2>' ,
+	'before_title' => '<div class="panel-heading"><h3 class="panel-title rounded">' ,
+	'after_title' => '</h3></div><div class="panel-body">' ,
     ) );
 
     register_sidebar( array(
 	'name' => 'Widgipage Second Column' ,
 	'id' => 'widgipage-col-2' ,
-	'before_widget' => '<div class="widgipage-col-2-widget widget">' ,
+	'before_widget' => '<div class="panel panel-default panel-widget">' ,
 	'after_widget' => '</div>' ,
-	'before_title' => '<h2 class="rounded">' ,
-	'after_title' => '</h2>' ,
+	'before_title' => '<div class="panel-heading"><h3 class="panel-title rounded">' ,
+	'after_title' => '</h3></div><div class="panel-body">' ,
     ) );
 
     register_sidebar( array(
@@ -63,3 +63,28 @@ function widgistrap_widgets_init() {
 }
 
 add_action( 'widgets_init' , 'widgistrap_widgets_init' );
+
+/**
+ * Register the Theme Customiser Stuff
+ * @param type $wp_customize
+ */
+function widgistrap_customize_register( $wp_customize ) {
+    //Section: Colour Scheme
+    $wp_customize -> add_section( 'widgistrap_color_scheme' , array(
+	'title' => __( 'Color Scheme' , 'widgistrap' ) ,
+	'priority' => 35 ,
+    ) );
+
+//    $wp_customize -> add_setting( 'widgistrap_theme_options[color_scheme]' , array(
+//	'default' => 'some-default-value' ,
+//	'type' => 'option' ,
+//	'capability' => 'edit_theme_options' ,
+//    ) );
+    //Section: Logo
+    $wp_customize -> add_section( 'widgistrap_logo' , array(
+	'title' => __( 'Logo Image' , 'widgistrap' ) ,
+	'priority' => 36 ,
+    ) );
+}
+
+add_action( 'customize_register' , 'widgistrap_customize_register' );
