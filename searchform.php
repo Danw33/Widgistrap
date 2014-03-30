a88584512685f633a10537536459f6a4b6e18f36
Daniel Wilson <dan.wilson@acuica.co.uk>
2014-03-31 00:16:35 +0100

    Further updates to sidebar and searchform widget output, although there is still a rouge `</div>` coming out from somewhere...

--- searchform.php
+++ searchform.php
@@ -1,6 +1,11 @@
-<form class="form search-form" role="search" method="get" action="<?php echo home_url('/'); ?>">
-    <div class="form-group">
-        <input type="text" class="form-control search-field" value="" name="s" placeholder="<?php echo __('Search', 'widgistrap'); ?>">
+<div class="panel panel-default panel-widget search-form">
+    <div class="panel-heading"><h3 class="panel-title rounded">Search</h3></div>
+    <div class="panel-body">
+	<form class="form search-form" role="search" method="get" action="<?php echo home_url( '/' ); ?>">
+	    <div class="form-group">
+		<input type="text" class="form-control search-field" value="" name="s" placeholder="<?php echo __( 'Search' , 'widgistrap' ); ?>">
+	    </div>
+	    <button type="submit" class="btn btn-default search-submit"><?php echo __( 'Search' , 'widgistrap' ); ?></button>
+	</form>
     </div>
-    <button type="submit" class="btn btn-default search-submit"><?php echo __('Search', 'widgistrap'); ?></button>
-</form>
+</div><!-- /.search-form -->
--- sidebar.php
+++ sidebar.php
@@ -1,11 +1,9 @@
 <aside id="sidebar" role="complementary">
     <div id="primary" class="widget-area">
-	<ul class="widget-area-list">
-	    <?php if( ! dynamic_sidebar( 'primary-widget-area' ) ) : ?>
-    	    <li id="search" class="primary-widget-area-widget widget widget-container widget_search">
-		    <?php get_search_form(); ?>
-    	    </li>
-	    <?php endif; ?>
-	</ul>
+	<?php
+	if( ! dynamic_sidebar( 'primary-widget-area' ) ) :
+	    get_search_form();
+	endif;
+	?>
     </div>
 </aside>
