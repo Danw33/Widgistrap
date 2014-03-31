64c45f96a2db3a8fb2e772b8aa1e08827241b4d9
Daniel Wilson <dan.wilson@acuica.co.uk>
2014-03-31 01:31:56 +0100

    Minor footer changes

--- footer.php
+++ footer.php
@@ -1,9 +1,9 @@
 <footer id="footer" role="contentinfo">
-    <div id="copyright">
-	<?php
-	echo sprintf( __( '%1$s %2$s %3$s. All Rights Reserved.' , 'widgistrap' ) , '&copy;' , date( 'Y' ) , esc_html( get_bloginfo( 'name' ) ) );
-	echo ' ' . sprintf( __( 'Theme By: %1$s.' , 'widgistrap' ) , '<a href="http://www.danielwilson.me/">Daniel Wilson</a>' );
-	?>
+    <div class="container footer-container">
+	<p class="text-muted"><?php
+	    echo sprintf( __( '%1$s %2$s %3$s. All Rights Reserved.' , 'widgistrap' ) , '&copy;' , date( 'Y' ) , esc_html( get_bloginfo( 'name' ) ) );
+	    echo ' ' . sprintf( __( 'Theme By %1$s.' , 'widgistrap' ) , '<a href="http://www.danielwilson.me/">Daniel Wilson</a>' );
+	    ?></p>
     </div>
 </footer>
 </div><!-- /#outer-container -->
